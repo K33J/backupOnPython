724ecd88bb7653471b768981ea5f0b5835ce9b03
user <example@protonmail.com>
2020-06-19 15:07:45 +0300

    Second version program

--- backup.py
+++ backup.py
@@ -1,47 +1,47 @@
-import os
-import time
-
-source = ['"/home/user/Pictures/testPng"']
-
-direction = '/home/user/Documents'
-
-target = direction + os.sep + time.strftime('%Y%m%d%H%M%S') + '.zip'
-
-zipCommand = "zip -qr {0} {1}" . format(target, ' '.join(source))
-
-if os.system(zipCommand) == 0:
-    print('Backup created succesfully! in ', target)
-else:
-    print('Error backup create..')
-##################################################################################################
-
-##################################################################################################
 # import os
 # import time
 
-# source = ['"/home/user/Pictures/wall"', '/home/user/Pictures/prof']
+# source = ['"/home/user/Pictures/testPng"']
 
 # direction = '/home/user/Documents'
 
-# today = direction + os.sep + time.strftime('%Y%m%d')
-# now = time.strftime('%H%M%S')
+# target = direction + os.sep + time.strftime('%Y%m%d%H%M%S') + '.zip'
 
-# target = today + os.sep + now + '.tar'
+# zipCommand = "zip -qr {0} {1}" . format(target, ' '.join(source))
 
-# comment = input('Put your commentary --> ')
-# if len(comment) == 0:
-#     target = today + os.sep + now + '.tar'
-# else:
-#     target = today + os.sep + now + '_' + \
-#         comment.replace(' ', '_') + '.tar'
-
-# if not os.path.exists(today):
-#     os.mkdir(today)
-#     print("Directory has created succesfull!..", today)
-
-# tarCommand = "tar cvf {0} {1}" . format(target, ' '.join(source))
-
-# if os.system(tarCommand) == 0:
+# if os.system(zipCommand) == 0:
 #     print('Backup created succesfully! in ', target)
 # else:
 #     print('Error backup create..')
+##################################################################################################
+
+##################################################################################################
+import os
+import time
+
+source = ['"/home/user/Pictures/wall"', '/home/user/Pictures/prof']
+
+direction = '/home/user/Documents'
+
+today = direction + os.sep + time.strftime('%Y%m%d')
+now = time.strftime('%H%M%S')
+
+target = today + os.sep + now + '.tar'
+
+comment = input('Put your commentary --> ')
+if len(comment) == 0:
+    target = today + os.sep + now + '.tar'
+else:
+    target = today + os.sep + now + '_' + \
+        comment.replace(' ', '_') + '.tar'
+
+if not os.path.exists(today):
+    os.mkdir(today)
+    print("Directory has created succesfull!..", today)
+
+tarCommand = "tar cvf {0} {1}" . format(target, ' '.join(source))
+
+if os.system(tarCommand) == 0:
+    print('Backup created succesfully! in ', target)
+else:
+    print('Error backup create..')
